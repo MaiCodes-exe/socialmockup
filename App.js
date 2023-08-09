8b38f62637bd035a48904c6ad05a999cc0bf8012
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-09 21:32:24 +0100

    added navbar to the import list

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import SideBar from './sidebar';
 import Feed from './feed';
 import RightBar from './rightbar';
 import Footer from './footer';
+import NavBar from './navbar';
 
 
 
 function App() {
   return (
     <div>
+      <NavBar></NavBar>
       <SideBar></SideBar>
       <Feed></Feed>
       <RightBar></RightBar>
