929e4836a9dad4a775960b490db47296d75b9dfe
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-09 21:24:25 +0100

    adding compinents to appjs file

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 
 import React from 'react';
+import SideBar from './sidebar';
+import Feed from './feed';
+import RightBar from './rightbar';
+import Footer from './footer';
 
 
 
 function App() {
   return (
     <div>
-     
+      <SideBar></SideBar>
+      <Feed></Feed>
+      <RightBar></RightBar>
+      <Footer></Footer>
     </div>
   );
 }
