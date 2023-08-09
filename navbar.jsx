44a0337abac6bc334b530fe1c39fcd72b232033f
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-09 21:31:46 +0100

    adding function to navabr

--- navbar.jsx
+++ navbar.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
 
-const HeroSection = () => {
+const NavBar = () => {
   return (
-    <Box mt={4} textAlign="center">
-      <Typography variant="h2">Landing Page</Typography>
-      <Typography variant="subtitle1">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.    </Typography>
-      <Button variant="contained" color="primary">
-        Learn More
-      </Button>
-    </Box>
+    <div></div>
   );
 };
 
-export default HeroSection;
+export default NavBar;
