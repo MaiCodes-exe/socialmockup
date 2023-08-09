5b519d31d364153c786e8d9ec6a622232d9bd666
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-09 20:00:08 +0100

    removing unused components

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 
 import React from 'react';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
-import HeroSection from './herosection';
-import Footer from './footer';
+
 
 
 function App() {
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Working on a title</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <HeroSection />
-      </Container>
-      <Footer />
+     
     </div>
   );
 }
