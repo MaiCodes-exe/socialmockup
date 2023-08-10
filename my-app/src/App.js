521df745823c1e60a87558020e3de04482a6a887
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-10 16:29:10 +0100

    adding new components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import SideBar from './components/sidebar';
-import Feed from './components/feed';
-import RightBar from './components/rightbar';
-import Footer from './components/footer';
-import NavBar from './components/navbar';
+// import React from 'react';
+// import SideBar from './components/sidebar';
+// import Feed from './components/feed';
+// import RightBar from './components/rightbar';
+// import Footer from './components/footer';
+import NavBar from ' ./components/navbar';
 import { Box, Stack } from '@mui/material';
 
 
@@ -13,10 +13,10 @@ function App() {
     <Box>
       <Stack direction="row" spacing={3} justifyContent={'center'}>
       <NavBar></NavBar>
-      <SideBar></SideBar>
+      {/* <SideBar></SideBar>
       <Feed></Feed>
       <RightBar></RightBar>
-      <Footer></Footer>
+      <Footer></Footer> */}
       </Stack>
     </Box>
   );
