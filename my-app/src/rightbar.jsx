f6c93904faf3dbc3e2459479bcf0c202aceeb6dd
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-15 16:02:32 +0100

    added images to image list component

--- my-app/src/rightbar.jsx
+++ my-app/src/rightbar.jsx
@@ -28,23 +28,41 @@ const RightBar = () => {
         <ImageList cols={3} rowHeight={100} gap={5}>
           <ImageListItem>
             <img 
-            src=""
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.KwoYsNz9OWU5j3dIuZbcLQHaEo%26pid%3DApi&f=1&ipt=9d3b38d564f22ba669d95a966a3c9b9fe2722af17be94e74716e2bb82183352a&ipo=images"
 
             />
           </ImageListItem>
         <ImageListItem>
             <img 
-            src=""
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.duuvY57GsRA9whXTsZ6regHaE8%26pid%3DApi&f=1&ipt=91f62a13832e24d6a0343d228d4e5d3fa21d5e3c45b869ee747110d581e3df3b&ipo=images"
+            
+            />
+          </ImageListItem>
+
+          <ImageListItem>
+            <img 
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP._3eSytAmdxDkD1204dcYCAHaE8%26pid%3DApi&f=1&ipt=549152643ffbc17277a07a21fd405ff551062e4e6681d5735c5793efebdf4510&ipo=images"
             
             />
           </ImageListItem>
           <ImageListItem>
             <img 
-            src=""
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.kRwXCTf9XR-hCqP4-c_1owHaFI%26pid%3DApi&f=1&ipt=90469450257982f55b1ff9f40b84948e0c579abd919faa1cfd30c756e1cbbf50&ipo=images"
             
             />
           </ImageListItem>
-
+          <ImageListItem>
+            <img 
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Wi8v5f6VqV0K0CZoFSZZzQHaE8%26pid%3DApi&f=1&ipt=0a9c79796ade6a8df938521ae720172b00743ede740375d81b52ee63a8f88cec&ipo=images"
+            
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img 
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.osmPNdu3wLCnIys8dQxMKgHaE8%26pid%3DApi&f=1&ipt=e608de17255a5900116415d4ed2d433062c194a6128cd0f793ded6d4fb59fd93&ipo=images"
+            
+            />
+          </ImageListItem>
         </ImageList>
         <Typography variant='h6' fontWeight={100} mt={2} mb={2} >Latest Posts</Typography>
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
