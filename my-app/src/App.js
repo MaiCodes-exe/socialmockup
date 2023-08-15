ff4bf05bd36d7b2516a3f95a97e7ff9fa05dc08a
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-15 18:42:31 +0100

    working on dark theme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SideBar from './sidebar';
 import Feed from './feed';
 import RightBar from './rightbar';
 import NavBar from './navbar';
-import { Box, Stack } from '@mui/material';
+import { Box, Stack, ThemeProvider } from '@mui/material';
 
 import Add from './add';
 
 function App() {
+  const [mode,setMode] = useState("light")
+
+  const darkTheme = createTheme({
+    palette:{
+      mode: mode
+    }
+  })
   return (
+    <ThemeProvider theme={darkTheme}> 
+
     <Box>
       <NavBar></NavBar>
-      <Stack direction="row" spacing={3} justifyContent={'space-evenly'}>
+      <Stack direction="row" spacing={2} justifyContent={'space-evenly'}>
       <SideBar></SideBar>
       <Feed></Feed>
       <RightBar></RightBar>
       </Stack>
       <Add/>
     </Box>
+    </ThemeProvider>
   );
 }
 
