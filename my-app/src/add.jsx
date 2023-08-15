d0e985e681409816b1c077cdc3cb25e29df49847
MaiCodes-exe <81877052+MaiCodes-exe@users.noreply.github.com>
2023-08-15 16:49:49 +0100

    building a modal

--- my-app/src/add.jsx
+++ my-app/src/add.jsx
@@ -1,17 +1,53 @@
 import React from 'react'
-import { Tooltip } from '@mui/material'
+import { Tooltip, Modal, Box, Typography} from '@mui/material'
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  height: 200,
+  width: 500,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 
 export const Add = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
   return (
     <>
-        <Tooltip title="Delete" sx={{position:"fixed", bottom: 20, left:{xs:"calc(50% - 20px)", md:30}}}>
+        <Tooltip onClick={e=>setOpen(true)}
+        title="Delete" sx={{position:"fixed", bottom: 20, left:{xs:"calc(50% - 20px)", md:30}}}>
             <Fab color="primary" aria-label="add">
                 <AddIcon />
             </Fab>
         </Tooltip>
+        <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={e=>setOpen(false)}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400}}>
+          <Typography> Create a Post</Typography>
+        </Box>
+      </Modal>
+    </div>
+
     </>
   )
    
